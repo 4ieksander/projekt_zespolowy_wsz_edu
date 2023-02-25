230d687c8f7173859fd45f194a1f506c5bf2a541
AleksanderOkrasa <olekokrasa@gmail.com>
2023-02-25 19:09:14 +0100

    Create a blank django template with connect to local database support

---
+++ team_project_django/international_organ_players/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InternalOrganPlayersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'international_organ_players'
