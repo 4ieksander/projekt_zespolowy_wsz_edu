2aa5627821b47500278fe5c45785ea9dfc25fd17
AleksanderOkrasa <olekokrasa@gmail.com>
2023-03-20 17:59:25 +0100

    edit Login and Logout View, now Login return token, user id and login user to session django

--- team_project_django/international_organ_players/views.py
+++ team_project_django/international_organ_players/views.py
@@ -11,6 +11,9 @@ from rest_framework import status
 from rest_framework import permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .models import (
     PlayerAttributes, Booster, Procedure, Equipment, Player, 
     OrganDonor, OrganData, OrganRecipient, MedicalStaff, 
@@ -23,7 +26,7 @@ from .serializers import (
     OrganRecipientSerializer, MedicalStaffSerializer, TechnicalStaffSerializer, 
     VehicleStorageAreaSerializer, VehicleSerializer, LocationSerializer,
     OrganStorageAreaSerializer, ClinicSerializer, OrganPossessedSerializer,
-    UserSerializer, TokenSerializer
+    UserSerializer, TokenSerializer, LoginSerializer, LogoutSerializer
 )
 
 
@@ -31,26 +34,37 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
 
-
-class LoginView(ObtainAuthToken):
-    serializer_class = TokenSerializer
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginView, self).post(request, *args, **kwargs)
-
-
-class LogoutView(generics.GenericAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
 
     def post(self, request):
-        logout(request)
-        return Response(status=status.HTTP_200_OK)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        password = serializer.validated_data['password']
+        if authenticate(username=user, password=password):
+            login(request, user=user)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user_id': token.user.id}, status=status.HTTP_200_OK)
+    
+    
+class LogoutView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = LogoutSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)    
+
+# class LogoutView(generics.GenericAPIView):
+#     permission_classes = (permissions.IsAuthenticated,)
+
+#     def post(self, request):
+#         logout(request)
+#         return Response(status=status.HTTP_200_OK)
 
 class PlayerList(generics.ListCreateAPIView):
     queryset = Player.objects.all()
